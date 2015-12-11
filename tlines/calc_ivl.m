934b1263348d08912d356e96d19fd51b8a74addd
Dmitry Smirnov <dsmi@outlook.com>
2015-12-11 13:48:41 -0700

    Purify calc_ivl

--- tlines/calc_ivl.m
+++ tlines/calc_ivl.m
@@ -12,45 +12,45 @@ function i = calc_ivl(tl,zobs,iobs,jsrc,lsrc)
 %   jsrc - source tline index.
 %   lsrc - linear source coefficient
 
-% Flag indicating that source and observation are the same.
-self = iobs==jsrc;
-
 if iobs<jsrc,
     % Find voltage at the left terminal of the source tline
     vt = calc_vvl(tl,tl.z(:,jsrc),jsrc,jsrc,lsrc);
     % Next, find voltage at the right terminal of the observation tline
-    V2 = vt.*prod(tl.Tls(:,iobs+1:jsrc-1),2);
-    % And voltage at its left terminal.
-    V1 = V2.*tl.Tls(:,iobs);
+    vr = vt.*prod(tl.Tls(:,iobs+1:jsrc-1),2);
+    % Finally, find current at the observation
+    i = vr.*calc_i_vterm(tl,zobs,iobs);
 elseif iobs>jsrc,
     % Find voltage at the right terminal of the source tline
     vt = calc_vvl(tl,tl.z(:,jsrc+1),jsrc,jsrc,lsrc);
     % Next, find voltage at the left terminal of the observation tline
-    V1 = vt.*prod(tl.Tgr(:,jsrc+1:iobs-1),2);
-    % And voltage at its right terminal
-    V2 = V1.*tl.Tgr(:,iobs);
+    vl = vt.*prod(tl.Tgr(:,jsrc+1:iobs-1),2);
+    % Finally, find current at the observation
+    i = vl.*calc_i_vleft(tl,zobs,iobs);
 else
+
+    % Voltages at the left and right terminal
     V1 = calc_vvl(tl, tl.z(:,iobs), iobs, jsrc, lsrc);
     V2 = calc_vvl(tl, tl.z(:,iobs+1), iobs, jsrc, lsrc);
+
+    t1 = tl.t1(:,iobs);
+    k = tl.k(:,iobs);
+    k2 = k.^2;
+    Y0 = tl.Y0(:,iobs);
+    Z = tl.k(:,iobs).*tl.Z0(:,iobs); % per-length impedance
+
+    % Forward and backward voltage waves - the z coordinate is shifted such that
+    % z=0 at the left terminal
+    Vm = (-V2 + V1.*t1 + lsrc./k2.*(t1-1))./(t1 - 1./t1);
+    Vp = V1 - Vm + lsrc./k2;
+
+    % Forward and backward current waves
+    Ip = Y0.*Vp;
+    Im = -Y0.*Vm;
+
+    % Shifted z - distance from the beginning of the observation line
+    z = zobs-tl.z(:,iobs);
+
+    % Finally the current at the observation
+    i = Ip.*exp(-k.*z) + Im.*exp(k.*z) + lsrc.*z./Z;
+
 end
-
-t1 = tl.t1(:,iobs);
-k = tl.k(:,iobs);
-k2 = k.^2;
-Y0 = tl.Y0(:,iobs);
-Z = tl.k(:,iobs).*tl.Z0(:,iobs); % per-length impedance
-
-% Forward and backward voltage waves - the z coordinate is shifted such that
-% z=0 at the left terminal
-Vm = (-V2 + V1.*t1 + lsrc*self./k2.*(t1-1))./(t1 - 1./t1);
-Vp = V1 - Vm + lsrc*self./k2;
-
-% Forward and backward current waves
-Ip = Y0.*Vp;
-Im = -Y0.*Vm;
-
-% Shifted z - distance from the beginning of the observation line
-z = zobs-tl.z(:,iobs);
-
-% Finally the current at the observation
-i = Ip.*exp(-k.*z) + Im.*exp(k.*z) + lsrc*self.*z./Z;
