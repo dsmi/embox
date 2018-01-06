9e896887f645e9e68d61413135e552b267390dca
Dmitry Smirnov <dsmi@outlook.com>
2018-01-06 15:56:14 -0700

    Update upper frequency in Debye dielectric model

--- common/debye.m
+++ common/debye.m
@@ -4,7 +4,7 @@ function eps = debye(er, lt, fref, f)
 % fref - frequency at which er, lt are measured
 
 fmin = 1.0e4;
-fmax = 1.0e12;
+fmax = 1.0e14;
 de = -er*lt*log(fmax/fmin)/(atan2(fref,fmax) - atan2(fref,fmin));
 einf = er - 0.5*de/log(fmax/fmin)*log((fmax*fmax + fref*fref)/(fmin*fmin + fref*fref));
 
