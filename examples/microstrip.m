3c8e9bdac08b2058dd9625a1cf1cf23bcc9365a9
Dmitry Smirnov <dsmi@outlook.com>
2018-01-06 15:57:21 -0700

    Microstrip example updated

--- examples/microstrip.m
+++ examples/microstrip.m
@@ -4,29 +4,61 @@
 
 addpath(genpath([ pwd, '/..' ]));
 
-function [ Y I wg mesh ]=simline(freq, nx, ny, a, b, h, weps, w, nl)
+mil2meter = 2.54e-5;
+
+% Dimensions
+lnpar.w = 12*mil2meter;   % stripline width
+lnpar.l = lnpar.w*16;     % stripline length
+lnpar.t = 1.35*mil2meter; % stripline thickness
+lnpar.d = 10*mil2meter;   % height above ground
+
+% Mesh options
+lnpar.nl = 5;     % number of the metal layers in the stripline mesh
+lnpar.nx = 32;    % cells along x
+lnpar.ny = 128;   % cells along y
+lnpar.a  = lnpar.l; % x-size of the waveguide
+lnpar.b  = lnpar.a; % y-size of the waveguide
+
+function [ Y I wg mesh ] = simline(freq, lnpar)
+
+    % Layers stack
+    h = [ lnpar.d  lnpar.d  repmat(lnpar.t/lnpar.nl, 1, lnpar.nl) ];
+
+    % Layer of copper at the bottom is the ground - it is not an ideal conductor
+    epsc = eps0 - j*ccustom/freq; % permittivity for copper
+    epsd = eps0 * debye(4.3, 0.02, 1e9/(2*pi), freq); % dielectric
+    weps = [ epsc  epsd  repmat(eps0, 1, lnpar.nl) ];
 
     % parametes of the enclosure to pass to mkzmat
-    wg      = wgparams(freq,a,b,h,nx,ny);
+    nx      = lnpar.nx;
+    ny      = lnpar.ny;
+    wg      = wgparams(freq, lnpar.a, lnpar.b, h, nx, ny);
     wg.weps = weps; 
-    wg.Gls0 = 0; % no bottom ground
     wg.Ggr0 = 0; % no top ground
+    wg.Gls0 = 0; % no bottom ground (we have layer of copper)
+    wg.cnx  = 4; % to speed up things
+    wg.cny  = 4;
 
     % Mesh cell size
-    dy = wg.b/ny;
+    dy = wg.b/lnpar.ny;
+
+    w = lnpar.w;
+    nl = lnpar.nl;
 
     % Make mesh from the layers
-    clear mesh
-    for lidx=1:nl
+    mesh.layers = struct( [] );
+    for li=1:lnpar.nl
         % x-directed trace
-        B0 = zeros(nx+2,ny+2);
-        B = drawline(B0, 0, (ny+2)/2, nx+2, (ny+2)/2, w/dy);
-        if lidx > 1 && lidx < nl
-            B = B - drawline(B0, 0, (ny+2)/2, nx+2, (ny+2)/2, w/dy - 2);
+        B0 = zeros(nx+2, ny+2);
+        BL = drawline(B0, 0, (ny+2)/2, nx+2, (ny+2)/2, w/dy);
+        BV = B0;
+        if li > 1
+            BV = traceedges(BL); % vias
         end
-        layer = mklayer(B);
-        layer.pos = lidx + 1;
-        mesh.layers(lidx) = layer;
+        if li > 1 && li < nl
+            BL = traceedges(BL); % only edges at intermediate layers
+        end
+        mesh.layers(end + 1) = mklayer(BL, BV, li + 1, ccopper);
     end
     
     % Identify ports
@@ -38,64 +70,80 @@ function [ Y I wg mesh ]=simline(freq, nx, ny, a, b, h, weps, w, nl)
     [ Y I ]=solvey(wg, mesh, ports, portw);
 end
 
-w=5.0e-5;   % stripline width
-l=2.0e-3;   % stripline length
-t=1.5e-5;   % stripline thickness
-d=3e-5;     % height above ground
-nl=2;       % number of the metal layers in the stripline model
-nx=16;      % cells along x
-ny=64;      % cells along y
-a=l;        % x-size of the waveguide
-b=w*8;      % y-size of the waveguide
+% Obtain port discontinuity matrix via simulation of the calibration
+% standards of single and double lengths
+function [ Ad R Y1 Y2 ] = calcporta(freq, lnpar)
 
-% angular frequencies
-%freqs = logspace(6,11,300)*2*pi;
-freqs = linspace(1e6,1e11,300)*2*pi;
-freqs = [ 1e9 1e10 1e11 ]*2*pi;
+    % signle-l
+    lnpar1 = lnpar;
+    lnpar1.a  = lnpar.a/8;
+    lnpar1.nx = lnpar.nx/8;
+    Y1 = simline(freq, lnpar1);
 
-% Simulation results
-Y1f = [];
-Y2f = [];
-Yf = [];
-
-for freq = freqs
-
-    step = find(freqs == freq);
-    steps = length(freqs);
-    fprintf( 'Solving for frequency %.8e, step %i of %i\n', freq, step, steps );
-
-    % Layer of copper at the bottom is the ground - it is not an ideal conductor
-    epsc = eps0 - j*ccopper/freq; % permittivity for copper - incorporates conductivity
-    h    = [ d     d     repmat(t/(nl-1), 1, nl-1)  d    ]; 
-    weps = [ epsc  eps0  repmat(eps0,     1, nl-1)  eps0 ];
-
-    % simulate l and 2*l to deembed
-    [ Y1 I1 wg mesh ] = simline(freq, nx,   ny, a,   b, h, weps, w, nl);
-    [ Y2 I2 wg mesh ] = simline(freq, nx*2, ny, a*2, b, h, weps, w, nl);
+    % double-l
+    lnpar2 = lnpar;
+    lnpar2.a  = lnpar.a/4;
+    lnpar2.nx = lnpar.nx/4;
+    Y2 = simline(freq, lnpar2);
 
     A1=y2abcd(Y1);
     A2=y2abcd(Y2);
 
     Add = A1*inv(A2)*A1; % double port discontinuity
     N = size(Add,1)/2;  % number of ports on each side
-    Add11=Add(1:N,1:N);
-    Add12=Add(1:N,N+1:end);
-    Add21=Add(N+1:end,1:N);
-    Add22=Add(N+1:end,N+1:end);
-    Ad  = [ Add11 Add12 ; Add21*0.5 Add22 ]; % port discontinuity
-    A = inv(Ad)*A1*inv(Ad); % de-embedded line of length l
-    Y = abcd2y(A) % admittance from simulation
+    A = Add(1:N,1:N);
+    B = Add(1:N,N+1:end);
+    C = Add(N+1:end,1:N);
+    D = Add(N+1:end,N+1:end);
 
-    % No deembedding for a while
-    %% [ Y I wg mesh ] = simline(freq, nx, ny, a, b, h, weps, w, nl);
-    %% A = y2abcd(Y);
+    Ad  = [ A B ; C*0.5 D ]; % port discontinuity
+    
+    % Estimate accuracy of deembedding.
+    % Expected values of the shunt ABCD
+    % are: A = 1; B = 0; C = 1/Zs; D = 1.0
+    R = abs( A - ones(N, N) ) + abs( B ) + abs( D - ones(N, N) );
+
+end
+
+% angular frequencies
+%freqs = logspace(6,11,300)*2*pi;
+freqs = linspace(1e7, 5e10, 50)*2*pi;
+
+% Simulation results
+Y1f = [];
+Y2f = [];
+Yndf = [];
+Yf = [];
+
+for ifr = 1:length(freqs)
+
+    %% step = find(freqs == freq);
+    %% steps = length(freqs);
+    freq = freqs(ifr);
+    fprintf( 'Solving for frequency %.8e, step %i of %i\n', freq, ifr, length(freqs) );
+
+    % deembedding simulations
+    [ Ad R Y1 Y2 ] = calcporta(freq, lnpar);
+
+    fprintf( 'De-embdeeing accuracy %.8e\n', sum(sum(R)) );
+    
+    % simulate line
+    [ Ynd I wg mesh ] = simline(freq, lnpar);
+
+    And = y2abcd(Ynd);
+
+    invAd = inv(Ad);
+    A = invAd * And * invAd;
+
+    Y = abcd2y(A);
 
     % Add y-parameters for this frequency to the overall results
     Yf = cat(3, Yf, Y);
 
-    % Capture y-parameters before de-embedding as well
-    %% Y1f = cat(3, Y1f, Y1);
-    %% Y2f = cat(3, Y2f, Y2);
+    % Capture y-parameters of the de-embedding standards and no-deembedded results
+    Yndf = cat(3, Yndf, Ynd);
+    Y1f = cat(3, Y1f, Y1);
+    Y2f = cat(3, Y2f, Y2);
 
     % Characteristic impedance and delay from simulation
     N=size(A,1)/2; % number of ports on each side
@@ -104,39 +152,23 @@ for freq = freqs
     Z0s=sqrt(A12*inv(A21))
     tds=acos(A(1,1))./freq
 
-    wavelen = 2*pi/(freq * sqrt(eps0 * mu0))
-    skin_depth = sqrt(2./(freq*mu0*ccopper))
-
-    % Calculated using the 2d fieldsolver
-    C = 3.8918761e-011;
-    L = 2.8589042e-007;
-    Ro = 2.2986668e+001;
-    Rs = 3.9169897e-003;
-
-    Zo = j*freq*L + Ro + Rs*(1+j)*sqrt(freq/(2*pi)); % impedance per len
-    Yo = j*freq*C; % admittance per len
-    Z0 = sqrt(Zo/Yo)
-
-    gamma = sqrt(Zo*Yo); % propagation constant
-    vp = j*freq/gamma;
-    td = l/vp;
-
-    Z11 = Z0/tanh(gamma.*l);
-    Z12 = Z0/sinh(gamma.*l);
-    Ztl = [ Z11 Z12 ; Z12 Z11 ];
-    Ytl = inv(Ztl);
 end
 
-%% freqs_hz = freqs/(2*pi);
-%% writey2p('microstrip2mm_embox.y2p', freqs_hz, Yf);
-%% writey2p('microstrip2mm_embox_1.y2p', freqs_hz, Y1f);
-%% writey2p('microstrip2mm_embox_2.y2p', freqs_hz, Y2f);
+function writesp(fileName, freq, Y, t, r0)
+  tswrite(fileName, freq/(2*pi), renorms(y2s(Y), [ 1 1 ], [ 50 50 ]), 'S', 50)
+end
 
-%% [ Tri, X, Y, Z, C ] = mesh2tri(wg, mesh, I);
-%% trisurf(Tri, X, Y, Z, C);
-%% xlim([ 0  wg.a+3*(wg.a/nx) ])
-%% ylim([ 0  wg.b+3*(wg.b/ny) ])
-%% zlim([ d  2*d+t*2          ])
+writesp('microstrip_lossless.s2p', freqs, Yf);
+writesp('microstrip_lossless_nd.s2p', freqs, Yndf);
+writesp('microstrip_lossless_l1.s2p', freqs, Y1f);
+writesp('microstrip_lossless_l2.s2p', freqs, Y2f);
+
+%% [ Tri, X, Y, Z, C ] = mesh2tri(wg, mesh, I2(:,1));
+%% %trisurf(Tri, X, Y, Z, C);
+%% trimesh(Tri, X, Y, Z);
+%% xlim([ -(wg.a/wg.nx)  wg.a+2*(wg.a/wg.nx) ])
+%% ylim([ -(wg.b/wg.ny)  wg.b+2*(wg.b/wg.ny) ])
+%% zlim([  lnpar.d       sum(wg.h)           ])
 
 % The improved colormap
 %% MR=[0,0; 
