dc3fcfd0c423876d97a8a089ac910dd7b0483d4f
Dmitry Smirnov <dsmi@outlook.com>
2020-04-25 11:21:28 -0600

    Thick tline example udpated

--- examples/thick.m
+++ examples/thick.m
@@ -99,23 +99,6 @@ freqs = linspace(1e6,2e10,10)*2*pi;
 Yf = [];
 Yndf = [];
 
-function [ D, Y1, Y2 ] = deembsims( fsim1, fsim2 )
-
-    fprintf('Running deembedding-l simulation...\n')
-    Y1 = fsim1( );
-
-    fprintf('Running deembedding-l*2 simulation...\n')
-    Y2 = fsim2( );
-
-    A1 = y2abcd(Y1);
-    A2 = y2abcd(Y2);
-
-    % Obtain the port discontinuity
-    [ D dtol ] = splitd( A1*inv(A2)*A1 );
-    fprintf('Deembedding accuracy is %.8e\n', dtol)
-    
-end
-
 % angular frequency
 for freq = freqs
 
