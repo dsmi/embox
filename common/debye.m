ace8b0349da2120e4b7df6e2352b77a5c67a7ac6
Dmitry Smirnov <dsmi@outlook.com>
2020-04-30 11:27:28 -0600

    Modify debye dielectric properties calculation function so it can handle arrays

--- common/debye.m
+++ common/debye.m
@@ -5,7 +5,7 @@ function eps = debye(er, lt, fref, f)
 
 fmin = 1.0e4;
 fmax = 1.0e14;
-de = -er*lt*log(fmax/fmin)/(atan2(fref,fmax) - atan2(fref,fmin));
+de = -er.*lt*log(fmax/fmin)./(atan2(fref,fmax) - atan2(fref,fmin));
 einf = er - 0.5*de/log(fmax/fmin)*log((fmax*fmax + fref*fref)/(fmin*fmin + fref*fref));
 
 eps = einf + de/log(fmax/fmin).*log((fmax + i*f)./(fmin + i*f));   
